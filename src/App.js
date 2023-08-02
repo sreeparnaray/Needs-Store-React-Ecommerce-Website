77299061b3a1a599660f83fce9664eba68ff2b5f
SREEPARNA RAY <trsreeparna99@gmail.com>
2023-08-02 10:49:31 +0530

    Project upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
+import Cart from "./Cart";
+import SingleProduct from "./SingleProduct";
+import ErrorPage from "./ErrorPage";
+import { GlobalStyle } from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App=() => {
+
+  const theme = {
+    colors:{
+      bg:"#F6F8FA",
+      footer_bg:"#0a1435",
+      btn:"rgb(93, 235, 197)",
+      border: "rgba(93, 237, 191, 0.5)",
+      hr:"#ffffff",
+      gradient:"linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
+      shadow: "rgba(0,0,0,0.02) 0px 1px 3px 0px, rgba(27,31,35,0.15) 0px 0px 0px 1px;",
+      shadowSupport:"rgba(0,0,0,0.16) 0px 1px 4px",
+    },
+    media: {
+      mobile: "768px" ,
+      tab: "998px" ,   
+    },
+  };
+
+
+  return (
+    <ThemeProvider theme={theme}>
+    <Router>
+      <GlobalStyle/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />}/>
+        <Route path="/Products" element={<Products />}/>
+        <Route path="/Contact" element={<Contact />}/>
+        <Route path="/SingleProduct/:id" element={<SingleProduct />}/>
+        <Route path="/Cart" element={<Cart />}/>
+        <Route path="*" element={<ErrorPage/>}/>
+      </Routes>
+      <Footer />
+    </Router>
+    
+    </ThemeProvider>
+  );
+};
+
+export default App;
